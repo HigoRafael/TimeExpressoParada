d733a6d0fc2876d741a4bea50507e642c2c58a65
Higor Rafael <higorrafael390@gmail.com>
2022-05-16 23:48:55 -0400

    Versão para teste em produção.

--- TimeExpressoParada/Form1.Designer.cs
+++ TimeExpressoParada/Form1.Designer.cs
@@ -28,20 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.mtxtIniTimer = new System.Windows.Forms.MaskedTextBox();
+            this.mtxtFinalTimer = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Inicio (start):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Termino (terminus):";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnCalc.Location = new System.Drawing.Point(0, 111);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(212, 67);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "Calcular Tempo";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // mtxtIniTimer
+            // 
+            this.mtxtIniTimer.Location = new System.Drawing.Point(129, 22);
+            this.mtxtIniTimer.Mask = "00:00";
+            this.mtxtIniTimer.Name = "mtxtIniTimer";
+            this.mtxtIniTimer.Size = new System.Drawing.Size(46, 23);
+            this.mtxtIniTimer.TabIndex = 0;
+            this.mtxtIniTimer.KeyUp += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox1_KeyUp);
+            // 
+            // mtxtFinalTimer
+            // 
+            this.mtxtFinalTimer.Location = new System.Drawing.Point(129, 61);
+            this.mtxtFinalTimer.Mask = "00:00";
+            this.mtxtFinalTimer.Name = "mtxtFinalTimer";
+            this.mtxtFinalTimer.Size = new System.Drawing.Size(46, 23);
+            this.mtxtFinalTimer.TabIndex = 1;
+            this.mtxtFinalTimer.KeyUp += new System.Windows.Forms.KeyEventHandler(this.mtxtFinalTimer_KeyUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(212, 178);
+            this.Controls.Add(this.mtxtFinalTimer);
+            this.Controls.Add(this.mtxtIniTimer);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer Expresso Parada";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button btnCalc;
+        private MaskedTextBox mtxtIniTimer;
+        private MaskedTextBox mtxtFinalTimer;
     }
 }
--- TimeExpressoParada/Form1.cs
+++ TimeExpressoParada/Form1.cs
@@ -5,11 +5,57 @@ namespace TimeExpressoParada
         public Form1()
         {
             InitializeComponent();
+
         }
+        private void CalcularTimer(String horIniti, String horFinal)
+        {
+            try
+            {
+                TimeSpan intervalo = Convert.ToDateTime(horIniti) - Convert.ToDateTime(horFinal);
+                MessageBox.Show("Horas:  " + intervalo.Hours + " Minutos " + intervalo.Minutes);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Verifique o formato dos dados inseridos");
+            }
+        }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void maskedTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !String.IsNullOrEmpty(mtxtIniTimer.Text))
+            {
+                mtxtFinalTimer.Focus();
+            }
+        }
+
+        private void mtxtFinalTimer_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !String.IsNullOrEmpty(mtxtFinalTimer.Text))
+            {
+                btnCalc.Focus();
+            }
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CalcularTimer(mtxtIniTimer.Text, mtxtFinalTimer.Text);
+                mtxtIniTimer.Text = "";
+                mtxtFinalTimer.Text = "";
+                mtxtIniTimer.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error !");
+            }
         }
     }
 }
